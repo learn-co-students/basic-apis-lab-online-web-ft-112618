2d023e65ed45500f6cecc8b6391df5f0bd6cccb0
Joe Quattrone <joequattrone5@gmail.com>
2019-03-25 11:58:34 -0400

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,9 +1,25 @@
+require 'pry'
 class RepositoriesController < ApplicationController
+ROOT = 'https://api.github.com'
+
 
   def search
 
   end
 
   def github_search
-  end
+      client_id = "REPLACE ME"
+      secret = "REPLACE ME"
+
+      resp = Faraday.get("https://api.github.com/search/repositories") do |req|
+        req.params["q"] = params[:query]
+        req.params["client_id"] = client_id
+        req.params["client_secret"] = secret
+      end
+
+      body = JSON.parse(resp.body)
+      binding.pry
+      @results = body["items"]
+      render :search
+    end
 end
